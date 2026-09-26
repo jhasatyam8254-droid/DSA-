080ccf0ca6da16fdc01bbaaed1dfb07af00d9bd9
jhasatyam8254-droid <jhasatyam8254@gmail.com>
2026-09-26 19:25:07 +0530

    Time: 7 ms (25.39%), Space: 13.9 MB (92.51%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.cpp
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     void duplicateZeros(vector<int>& arr) {
-     int n = arr.size();
+        int n = arr.size();
 
-     for(int i = 0; i < n; i++){
-        if(arr[i] == 0){
-            for (int j = n-1; j > i; j--){
-                arr[j] = arr[j-1];
+        for(int i = 0; i < n - 1; i++) {
+            if(arr[i] == 0) {
+
+                for(int j = n - 1; j > i; j--) {
+                    arr[j] = arr[j - 1];
+                }
+
+                arr[i + 1] = 0;
+                i++;
             }
-
-            if(i+1 < n)
-            arr[i+1] = 0;
-            i++;
         }
-     }   
     }
 };
